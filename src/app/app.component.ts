3d8b34ed68dcbd15c6961f9592b356e28cf64a50
Аянбек Ш.И <ayanbekshynar@gmail.com>
2022-09-14 17:45:55 +0600

    angular library test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {LoggerService} from "my-logger";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-library';
+
+  constructor(private loggerService: LoggerService) {
+    this.loggerService.log('Hello, world!');
+  }
 }
